4a4ab8a3eb9c4ab8ec69ae2d29161777df2cdb3e
deepak-karkala <dkarkala01@gmail.com>
2020-03-30 11:34:53 +0530

    Bicycle physics explained: update

--- bicycle_physics_explained/code/scripts/script1.js
+++ bicycle_physics_explained/code/scripts/script1.js
@@ -11,17 +11,28 @@
 
 	// generic window resize listener event
 	function handleResize() {
-		// 1. update height of step elements
-		var stepH = Math.floor(window.innerHeight * 0.75);
-		step.style('height', stepH + 'px');
+		windowWidth = window.innerWidth;
 
-		var figureHeight = window.innerHeight * 0.8// / 2
-		var figureMarginTop = (window.innerHeight - figureHeight) / 2  
+		if (windowWidth >= 576) {
+			// 1. update height of step elements
+			var stepH = Math.floor(window.innerHeight * 0.75);
+			step.style('height', stepH + 'px');
+
+			var figureHeight = window.innerHeight * 0.85// / 2
+			var figureMarginTop = Math.max((window.innerHeight - figureHeight) / 2, 60);  
+
+		} else {
+			// 1. update height of step elements
+			var stepH = Math.floor(window.innerHeight * 0.40);
+			step.style('height', stepH + 'px');
+
+			var figureHeight = window.innerHeight * 0.55// / 2
+			var figureMarginTop = Math.max((window.innerHeight - figureHeight) / 2, 60);  
+		}
 
 		figure
 			.style('height', figureHeight + 'px')
 			.style('top', figureMarginTop + 'px');
-
 
 		// 3. tell scrollama to update new element dimensions
 		scroller.resize();
@@ -64,7 +75,7 @@
 			offset: 0.33,
 			debug: false,
 		})
-			.onStepEnter(handleStepEnter)
+		.onStepEnter(handleStepEnter)
 
 
 		// setup resize event
@@ -77,15 +88,79 @@
 
 	function handleStepTransition(data_step_id) {
 	    d3.select("#graphic").select("svg").remove();
+	    d3.select("#graphic_container_span").remove();
+	    div_graphic_container = document.getElementById("graphic_container");
 
-	    div_slider_text = document.getElementById("slider_text");
+		if (data_step_id == 0 || data_step_id == 1) {
+	    	div_graphic_container.innerHTML = `
+	    			<span class="graphic_container_span">
+	    				<div class="row">
+							<div class="col-lg-12 text-center slider_text" id="slider_text"></div>
+						</div>
+						<div class="row graphic_div">
+							<div class="col-lg-12" id="graphic"></div>
+						</div>
+					</span>`;
+		} else {
+			div_graphic_container.innerHTML = `
+	    			<span class="graphic_container_span">
+						<div class="row">
+							<div class="col-lg-12 text-center slider_text" id="slider_text"></div>
+						</div>
+						<div class="row slider_div">
+							<div class="col-lg-6 text-center mx-auto">
+								<div class="row">
+									<div class="col-lg-12 slider_tag" id="slider_tag"></div>
+								</div>
+								<div class="row">
+									<div class="col-lg-8 text-center mx-auto slider" id="slider"></div>
+								</div>
+							</div>
+						</div>
+						<div class="row graphic_div">
+							<div class="col-lg-12" id="graphic"></div>
+						</div>
+					</span>`;
+			div_slider = document.getElementById("slider");
+        	div_slider.innerHTML = '';
+        	div_slider_tag = document.getElementById("slider_tag");
+        	div_slider_tag.innerHTML = '';
+		}
+
+		div_slider_text = document.getElementById("slider_text");
         div_slider_text.innerHTML = '';
-        div_slider = document.getElementById("slider");
-        div_slider.innerHTML = '';
-        div_slider_tag = document.getElementById("slider_tag");
-        div_slider_tag.innerHTML = '';
+        
+	    if (data_step_id == 0) {
+	    	image = "images/racer.png";
+	    	//zoomTo = 
+	    	//show_bicycle_forces(image, idname, zoomTo);
 
-	    if (data_step_id == 1) {
+	    	div_slider_text.innerHTML = "Forces acting on a bicycle";
+	    	file = "data/bicycle_position_forces.csv";
+           	idname = "#graphic";
+			transition_duration = [0];
+			end_x = 10;
+			start_y_arr = [110];
+			end_y_arr = start_y_arr;
+			max_y = 90;
+			user_value = "";
+			type = "other";
+           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
+	    
+	    } else if (data_step_id == 1) {
+	    	div_slider_text.innerHTML = "Rolling resistance acting against a bicycle";
+	    	file = "data/forces_rolling.csv";
+           	idname = "#graphic";
+			transition_duration = [0];
+			end_x = 10;
+			start_y_arr = [110];
+			end_y_arr = start_y_arr;
+			max_y = 90;
+			user_value = "";
+			type = "other";
+           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
+
+	    } else if (data_step_id == 2) {
 	    	div_slider_text.innerHTML = "Use the slider to see how the rider's weight affects the speed of the bike";
 			div_slider.innerHTML = `<div id="slider-range-min"></div>`;
 
@@ -98,14 +173,27 @@
            	idname = "#graphic";
 			transition_duration = [0, 0];
 			end_x = 5;
-			start_y_arr = [70, 30];
-			end_y_arr = [70, 30];
+			start_y_arr = [80, 40];
+			end_y_arr = [80, 40];
 			max_y = 100;
 			user_value = initial_value + ' kg';
 			type = "rolling";
-           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type);
+           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
 
 	    } else if (data_step_id == 3) {
+	    	div_slider_text.innerHTML = "Force of gravity acting on a bicycle";
+	    	file = "data/forces_gravity.csv";
+           	idname = "#graphic";
+			transition_duration = [0];
+			end_x = 10;
+			start_y_arr = [110];
+			end_y_arr = start_y_arr;
+			max_y = 90;
+			user_value = "";
+			type = "other";
+           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
+
+	    } else if (data_step_id == 4) {
 	    	div_slider_text.innerHTML = "Use the slider to see how the slope affects the speed of the bike";
 			div_slider.innerHTML = `<div id="slider-range-min"></div>`;
 
@@ -125,116 +213,87 @@
 			max_y = 140;
 			user_value = initial_value + ' degrees';
 			type = "gravity";
-           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type);
+           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
 	    
 	    } else if (data_step_id == 5) {
-	    	div_slider_text.innerHTML = "Notice how the seating position affects the drag and thereby speed of the bike";
+	    	div_slider_text.innerHTML = "Aerodynamic drag acting against a bicycle";
+	    	file = "data/forces_drag.csv";
+           	idname = "#graphic";
+			transition_duration = [0];
+			end_x = 10;
+			start_y_arr = [110];
+			end_y_arr = start_y_arr;
+			max_y = 90;
+			user_value = "";
+			type = "other";
+           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
 
-	    	/*
-	    	div_slider_tag.innerHTML = `<div class="row">
-	    									<div class="col-lg-4">
-	    										<div class="row">
-	    											<div class="col-lg-12">
-			    										<span class="drag_position">Relaxed</span>
-			    									</div>
-		    									</div>
-	    										<div class="row">
-	    											<div class="col-lg-12">
-			    										<img class="drag_image" src="images/casual.jpg">
-			    									</div>
-		    									</div>
-		    									<div class="row">
-	    											<div class="col-lg-12">
-			    										<span class="drag_value">C<sub>d</sub>.A = 5.4</span>
-			    									</div>
-		    									</div>
-	    									</div>
-	    									<div class="col-lg-4">
-	    										<div class="row">
-	    											<div class="col-lg-12">
-			    										<span class="drag_position">Aerodynamic</span>
-			    									</div>
-		    									</div>
-	    										<div class="row">
-	    											<div class="col-lg-12">
-			    										<img class="drag_image" src="images/aero.jpg">
-			    									</div>
-		    									</div>
-		    									<div class="row">
-	    											<div class="col-lg-12">
-			    										<span class="drag_value">C<sub>d</sub>.A = 3.2</span>
-			    									</div>
-		    									</div>
-	    									</div>
-	    									<div class="col-lg-4">
-	    										<div class="row">
-	    											<div class="col-lg-12">
-			    										<span class="drag_position">Superman</span>
-			    									</div>
-		    									</div>
-	    										<div class="row">
-	    											<div class="col-lg-12">
-			    										<img class="drag_image" src="images/superman.jpeg">
-			    									</div>
-		    									</div>
-		    									<div class="row">
-	    											<div class="col-lg-12">
-			    										<span class="drag_value">C<sub>d</sub>.A = 0.5</span>
-			    									</div>
-		    									</div>
-	    									</div>
-	    								</div>`;
-			*/
+	    } else if (data_step_id == 6) {
+	    	div_slider_text.innerHTML = "Notice how the seating position affects the drag and thereby speed of the bike";
 	    	file = "data/bicycle_position_drag.csv";
            	idname = "#graphic";
 			transition_duration = [0, 0, 0];
 			end_x = 10;
 			start_y_arr = [80, 50, 20];
 			end_y_arr = start_y_arr;
-			max_y = 100;
+			max_y = 90;
 			user_value = "";
 			type = "drag";
-           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type);
+           	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
            	set_button_drag();
 	    }
 	}
 
-    function plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr,  max_y, user_value, type) {
-	    d3.select(idname).select("svg").remove();
+	/*
+	function show_bicycle_forces() {
+		d3.select(idname).select("svg").remove();
 
 		var width_scale_factor = 0.80;
-		var margin = {right:20, left:20, top:10, bottom:10};
+		var margin = {right:5, left:5, top:10, bottom:10};
 
 		var bb = d3.select(idname).node().offsetWidth;
 		base_width = bb*width_scale_factor - margin.left - margin.right;
-		if (type=="rolling") {
-			var height_scale_factor = 0.5;
-		} else if (type=="gravity") {
-			var height_scale_factor = 0.5;
-		} else if (type=="drag") {
-			var height_scale_factor = 0.5;
+		windowWidth = window.innerWidth;
+	}
+	*/
+    function plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr,  max_y, user_value, type, data_step_id) {
+	    d3.select(idname).select("svg").remove();
+
+		var width_scale_factor = 0.80;
+		var margin = {right:5, left:5, top:10, bottom:10};
+
+		var bb = d3.select(idname).node().offsetWidth;
+		base_width = bb*width_scale_factor - margin.left - margin.right;
+		windowWidth = window.innerWidth;
+
+		if (windowWidth >= 576) {
+			if (type=="rolling") {
+				var height_scale_factor = 589/base_width * 1.2 * 0.5;
+			} else if (type=="gravity") {
+				var height_scale_factor = 589/base_width * 1.2 * 0.4;
+			} else if (type=="drag") {
+				var height_scale_factor = 589/base_width * 1.2 * 0.4;
+			} else {
+				var height_scale_factor = 589/base_width * 1.2 * 0.9;
+			}
+		} else {
+			if (type=="rolling") {
+				var height_scale_factor = 0.8;
+			} else if (type=="gravity") {
+				var height_scale_factor = 0.8;
+			} else if (type=="drag") {
+				var height_scale_factor = 0.8;
+			} else {
+				var height_scale_factor = 0.9;
+			}
 		}
 		base_height = bb*height_scale_factor - margin.top - margin.bottom;
 		
-		plot_bicycle_comparison(idname, file, base_width, base_height, margin, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type);
+		plot_bicycle_comparison(idname, file, base_width, base_height, margin, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id);
     }
 
 
-	function plot_bicycle_comparison(id, file, width, height, margin, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type) {
-
-		if (type == "rolling") {
-			line_start_y_arr = [55, 15];
-			line_end_y_arr = [55, 15];
-			max_x = 100;
-		} else if (type =="gravity") {
-			line_start_y_arr = [80, 20];
-			line_end_y_arr = [line_start_y_arr[0] + end_y_arr[0]-start_y_arr[0], line_start_y_arr[1] + end_y_arr[1]-start_y_arr[1]];
-			max_x = 100;
-		} else if (type =="drag") {
-			line_start_y_arr = [65, 35, 5];
-			line_end_y_arr = line_start_y_arr;
-			max_x = 90;
-		}
+	function plot_bicycle_comparison(id, file, width, height, margin, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type, data_step_id) {
 
 		var x = d3.scaleLinear()
 	    	  .range([0, width]);
@@ -256,22 +315,33 @@
 
 		    data.forEach(function(d) {
 		   		d.start_x = +d.start_x;
+
+		   		icon_width = width*0.20;
+		   		icon_height = height*0.20;
+
+				if (type == "rolling") {
+					line_start_y_arr = [60, 20];
+					line_end_y_arr = [60, 20];
+					max_x = 100;
+				} else if (type =="gravity") {
+					line_start_y_arr = [80, 20];
+					line_end_y_arr = [line_start_y_arr[0] + end_y_arr[0]-start_y_arr[0], line_start_y_arr[1] + end_y_arr[1]-start_y_arr[1]];
+					max_x = 100;
+				} else if (type =="drag") {
+					line_start_y_arr = [60, 30, 5];
+					line_end_y_arr = line_start_y_arr;
+					max_x = 90;
+				} else {
+					max_x = d.start_x;
+					icon_width = width*0.90;
+		   			icon_height = height*0.95;
+				}
 		    });
 
 		    x.domain([0, 100]);
 		    y.domain([0, max_y]);
 
-		    svg.selectAll(".line")
-		    	.data(data)
-		    	.enter()
-			    .append("line")
-		    	.style("stroke", "black")
-		    	.attr("x1", function(d) {return x(d.start_x);})
-		    	.attr("x2", function(d) {return x(max_x);})
-		    	.attr("y1", function(d, i) {return y(line_start_y_arr[i]); })
-		    	.attr("y2", function(d, i) {return y(line_end_y_arr[i]); });
-
-			var images = svg.selectAll(".images")
+		    var images = svg.selectAll(".images")
 							  .data(data)
 							  .enter()
 							  .append("image");
@@ -279,41 +349,73 @@
 			images.attr("xlink:href", function(d) {return d.url; })
 			  .attr("x", function(d) {return x(d.start_x); })
 			  .attr("y", function(d, i) {return y(start_y_arr[i]); })
-			  .attr("width", 60)
-			  .attr("height", 60)
+			  .attr("width", icon_width)
+			  .attr("height", icon_height)
 			  .transition()
 		      	.ease(d3.easeLinear)
 		      	.duration(function(d, i) { return transition_duration[i]; })
 		      	.attr("x", function(d) { return x(end_x); })
 		      	.attr("y", function(d, i) { return y(end_y_arr[i]); });
 
-		    var text = svg.selectAll(".text")
-                    .data(data)
-                    .enter()
-                    .append("text")
-                    .attr("x", function(d) { return x(d.start_x)-5; })
-                    .attr("y", function(d, i) { return y(start_y_arr[i]); })
-                    .style("fill", "black")
-                    .text(function(d,i) { return d.name;} )
-                        .transition()
-                        	.ease(d3.easeLinear)
-		      				.duration(function(d, i) { return transition_duration[i]; })
-                            .attr("x", function(d) { return x(end_x); })
-		     			 	.attr("y", function(d, i) { return y(end_y_arr[i]); })
-                            .text(function(d,i) {
-                            	if ((type == "rolling") || (type=="gravity")) {
-	                            	if (i==0) {
-	                            		return d.name;
-	                            	} else {
-	                            		return d.name + ' [' + user_value + ']'; 
-	                            	}
-	                            } else if (type == "drag") {
-	                            	return d.name;
-	                            }
-                            })
-                            .style("fill", "black");
+		    if (type != "other") {
+			    svg.selectAll(".line")
+			    	.data(data)
+			    	.enter()
+				    .append("line")
+			    	.style("stroke", "black")
+			    	.attr("x1", function(d) {return x(d.start_x);})
+			    	.attr("x2", function(d) {return x(max_x);})
+			    	.attr("y1", function(d, i) {return y(line_start_y_arr[i]); })
+			    	.attr("y2", function(d, i) {return y(line_end_y_arr[i]); });
+
+			    var text = svg.selectAll(".text")
+	                    .data(data)
+	                    .enter()
+	                    .append("text")
+	                    .attr("x", function(d) { return x(d.start_x)-5; })
+	                    .attr("y", function(d, i) { return y(start_y_arr[i]); })
+	                    .style("fill", "black")
+	                    .text(function(d,i) { return d.name;} )
+	                        .transition()
+	                        	.ease(d3.easeLinear)
+			      				.duration(function(d, i) { return transition_duration[i]; })
+	                            .attr("x", function(d) { return x(end_x); })
+			     			 	.attr("y", function(d, i) { return y(end_y_arr[i]); })
+	                            .text(function(d,i) {
+	                            	if ((type == "rolling") || (type=="gravity")) {
+		                            	if (i==0) {
+		                            		return d.name;
+		                            	} else {
+		                            		return d.name + ' [' + user_value + ']'; 
+		                            	}
+		                            } else if (type == "drag") {
+		                            	return d.name;
+		                            }
+	                            })
+	                            .style("fill", "black");
+	        }
 
 		});
+
+	  	/*
+		function transition(svg, start, end) {
+		  var center = [50, 45], //[width / 2, height / 2],
+		      i = d3.interpolateZoom(start, end);
+
+		  svg
+		      .attr("transform", transform(start))
+		    .transition()
+		      .delay(250)
+		      .duration(i.duration * 2)
+		      .attrTween("transform", function() { return function(t) { return transform(i(t)); }; });
+		      //.each("end", function() { d3.select(this).call(transition, end, start); });
+
+		  function transform(p) {
+		    var k = height / p[2];
+		    return "translate(" + (center[0] - p[0] * k) + "," + (center[1] - p[1] * k) + ")scale(" + k + ")";
+		  }
+		}
+		*/
 	}
 
 	function set_button_drag() {
@@ -355,10 +457,10 @@
 	      	W2 = ui.value;
 	      	t2 = W2 * t1 / W1;
 			transition_duration = [t1, t2];
-			end_x = 80;
+			end_x = 75;
 			max_y = 100;
-			start_y_arr = [70, 30];
-			end_y_arr = [70, 30];
+			start_y_arr = [80, 40];
+			end_y_arr = [80, 40];
 			user_value = W2 + ' kg';
 			type = "rolling";
            	plot_bicycle_comparison_top(file, idname, transition_duration, end_x, start_y_arr, end_y_arr, max_y, user_value, type);
